35a6a42ae89a01f6a6678e83ce6c6f7c23d890d5
RomLyt <121231829+RomLyt@users.noreply.github.com>
2023-05-13 10:45:27 +0300

    7-task-corect

--- js/task-07.js
+++ js/task-07.js
@@ -2,6 +2,5 @@ const textSize = document.querySelector('#font-size-control');
 const textArea = document.querySelector('#text')
 
 textSize.addEventListener('input', event => {
-    console.log(textSize.value)
     textArea.style.fontSize = `${textSize.value}px`;
 });
