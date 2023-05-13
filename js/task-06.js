a447e8224ee2b660faf19396d42091a8f78cd5b9
lytvynenkoroman84@gmail.com <121231829+RomLyt@users.noreply.github.com>
2023-05-13 12:56:37 +0300

    8-task

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,6 @@ const inputText = document.querySelector('input');
 
 inputText.addEventListener("blur", event => {
     let length = inputText.value.length;
-    console.log(length);
     if (length === 6) inputText.style.borderColor = "#4caf50";
     else inputText.style.borderColor = "#f44336";
 });
