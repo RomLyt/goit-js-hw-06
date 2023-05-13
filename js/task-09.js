32a6b44e534d03b532483a392e8344081d334904
lytvynenkoroman84@gmail.com <121231829+RomLyt@users.noreply.github.com>
2023-05-13 13:17:53 +0300

    hw-06

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const changeBtnEl = document.querySelector('.change-color');
+const indexColorEl = document.querySelector('.color');
+
+changeBtnEl.addEventListener('click', changeColor);
+
+function changeColor() {
+  bodyEl.style.color = getRandomHexColor();
+  indexColorEl.textContent = getRandomHexColor();
+}
